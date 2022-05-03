88e969d9e4f0e672ddec4aa39a07e4dc3ccc8026
Amit Cohen <cohen.amit.official@gmail.com>
2022-05-03 17:17:58 +0300

    Home page 1.3

--- Screens/HomePage.js
+++ Screens/HomePage.js
@@ -3,7 +3,7 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
 import LoginPage from './LoginPage';
-import { page } from "yarn/lib/cli";
+import {page} from 'yarn/lib/cli';
 //import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
